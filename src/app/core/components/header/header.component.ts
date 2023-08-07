95c15fca8ff7a6588b1ee3f2d01e888ee1155ccb
Leandro Luna <leandro.j.luna@gmail.com>
2023-08-06 23:10:14 -0300

    feat: :sparkles: fix data-theme, organize router-outlet and portfolio main component, and add logo

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   isDarkTheme = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
 
   toggleTheme() {
     this.isDarkTheme = !this.isDarkTheme;
     const theme = this.isDarkTheme ? 'dark' : 'light';
     document.body.setAttribute('data-theme', theme);
+    console.log('Theme changed to: ', theme);
+  }
+
+  onNavigate(route: string) {
+    this.router.navigate([route]);
   }
 }
