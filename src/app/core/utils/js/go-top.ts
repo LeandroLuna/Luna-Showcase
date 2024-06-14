84103637969188cc13ee04780a665b4e8923adba
Leandro Luna <leandro.j.luna@gmail.com>
2024-06-14 13:39:08 -0300

    feat: clone template repo

--- src/app/core/utils/js/go-top.ts
+++
@@ -1,8 +0,0 @@
-export default function goTop() {
-    // const goTopButton = document.getElementById('go-top');
-    // if (goTopButton) {
-    //     goTopButton.addEventListener('click', () => {
-    //     window.scrollTo({ top: 0, behavior: 'smooth' });
-    //     });
-    // }
-};
